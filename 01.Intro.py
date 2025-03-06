b2173421f514dd172e22afcda2b880cc33ee60e7
arindam9864 <arindam.9864@gmail.com>
2025-03-06 11:11:39 +0530

    Rename

---
+++ 01.Intro.py
@@ -0,0 +1,8 @@
+"""
+Python is a high-level, interpreted programming language.
+
+High Level:
+A high-level programming language means that Python is designed to be closer to human language and easier to read, write, and understand compared to low-level languages like assembly or machine code.
+
+ It was created by Guido van Rossum in 1991
+"""
